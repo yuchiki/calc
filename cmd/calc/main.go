95f74ba06f9a6ace413c6d52bd01e655775adf69
yuchiki <yuki08281993@gmail.com>
2020-12-04 22:23:04 +0900

    i+iが計算できた

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
+
+	parsec "github.com/prataprc/goparsec"
 )
 
 func main() {
 	flag.Parse()
 	expression := flag.Arg(0)
+	scanner := parsec.NewScanner([]byte(expression)).TrackLineno()
 
 	fmt.Println(expression)
+
+	plus := parsec.Atom("+", "PLUS")
+
+	ast := parsec.NewAST("expr", 1000)
+	parser := ast.And("plusExpr", nil, parsec.Int(), plus, parsec.Int())
+
+	node, scanner := ast.Parsewith(parser, scanner)
+
+	_ = node
+
+	ast.Prettyprint()
+
+	i1, _ := strconv.Atoi(node.GetChildren()[0].GetValue())
+	i3, _ := strconv.Atoi(node.GetChildren()[2].GetValue())
+
+	answer := i1 + i3
+	fmt.Println(answer)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yuchiki/calc
 
 go 1.13
+
+require github.com/prataprc/goparsec v0.0.0-20180806094145-2600a2a4a410
