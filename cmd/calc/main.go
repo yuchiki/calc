cee4401cac16f13b11a8edc2c41cdc1aff1c6183
yuchiki <yuki08281993@gmail.com>
2020-12-04 22:54:51 +0900

    refactoring

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -15,20 +15,29 @@ func main() {
 
 	fmt.Println(expression)
 
-	plus := parsec.Atom("+", "PLUS")
-
 	ast := parsec.NewAST("expr", 1000)
-	parser := ast.And("plusExpr", nil, parsec.Int(), plus, parsec.Int())
 
+	parser := makeParser(ast)
 	node, scanner := ast.Parsewith(parser, scanner)
 
 	_ = node
 
 	ast.Prettyprint()
 
+	answer := eval(node)
+	fmt.Println(answer)
+}
+
+func makeParser(ast *parsec.AST) parsec.Parser {
+	plus := parsec.Atom("+", "PLUS")
+	parser := ast.And("plusExpr", nil, parsec.Int(), plus, parsec.Int())
+	return parser
+}
+
+func eval(node parsec.Queryable) int {
 	i1, _ := strconv.Atoi(node.GetChildren()[0].GetValue())
 	i3, _ := strconv.Atoi(node.GetChildren()[2].GetValue())
 
 	answer := i1 + i3
-	fmt.Println(answer)
+	return answer
 }
